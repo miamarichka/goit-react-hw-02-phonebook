708a527e29d8ea178b0791f3293bc584e94834c7
miamarichka <ilchyshyn.marichka@gmail.com>
2023-03-22 22:08:43 +0300

    styled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,7 @@ import { ContactsList } from './ContactsList';
 
 export class App extends React.Component {
   state = {
-    contacts: [
-      {name: 'Rosie Simpson', number: '459-12-56' },
-      {name: 'Hermione Kline', number: '443-89-12' },
-      {name: 'Eden Clements', number: '645-17-79' },
-      {name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
